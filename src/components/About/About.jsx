80280f772541a63d955e3b08ed4d7ef1204cc45f
ryanalmasi <ryanalmasii@gmail.com>
2023-01-16 18:59:56 -0700

    Changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './About.scss'
 import { FaLaptopCode } from 'react-icons/fa'
+import { TbSchool } from 'react-icons/tb'
+import { MdOutlineWorkOutline } from 'react-icons/md'
 
 
 const About = () => {
   return (
     <section id="about">
       <header className="about-header">
-        <h1 className='about-h1'>ABOUT ME</h1>
+        <h1 className='about-h1'>ABOUT</h1>
         <h3 className='about-h3'>&lt; /&gt;</h3>
       </header>
       
@@ -21,23 +23,25 @@ const About = () => {
         <div className='about-cont_content'>
           <div className='about-cards'>
             <article className='about-card about-card-experience'>
-              <FaLaptopCode className='about-icon' />
-              <h2>EXPERIENCE</h2>
+              <MdOutlineWorkOutline className='about-icon' />
+              <h2 className='about-h2'>EXPERIENCE</h2>
               <small>~4 months professional experience</small>
             </article>
 
             <article className='about-card about-card-academic'>
-              <FaLaptopCode className='about-icon' />
-              <h2>ACADEMIC HISTORY</h2>
+              <TbSchool className='about-icon' />
+              <h2 className='about-h2'>ACADEMIC HISTORY</h2>
               <small>BSc Computer Engineering Co-op - Software Option</small>
             </article>
 
             <article className='about-card about-card-coop'>
               <FaLaptopCode className='about-icon' />
-              <h2>CO-OP WORK TERMS</h2>
+              <h2 className='about-h2'>CO-OP AVAILABILITY</h2>
               <small>4 months | Summer 2023</small>
             </article>
           </div>
+
+
         </div>
       </div>
     </section>
