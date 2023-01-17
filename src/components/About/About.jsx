b3eff230666638632edb48309dbfd3b59b8157e2
ryanalmasi <108089503+ryanalmasi@users.noreply.github.com>
2023-01-16 22:28:09 -0700

    Changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.scss'
+import IMAGE from '../../Assets/Images/IMG_1995.jpg'
 import { FaLaptopCode } from 'react-icons/fa'
 import { TbSchool } from 'react-icons/tb'
 import { MdOutlineWorkOutline } from 'react-icons/md'
@@ -16,7 +17,7 @@ const About = () => {
       <div className='container'>
         <div className='container about-cont'>
           <div className='about_img'>
-            <img alt='My Image' />
+            <img src={IMAGE} alt='My Image' />
           </div>
         </div>
 
@@ -40,9 +41,37 @@ const About = () => {
               <small>4 months | Summer 2023</small>
             </article>
           </div>
-
-
         </div>
+      </div>
+      <div className='paragraph-cont'>
+        <p className='paragraph paragraph-1'>
+            Developer since youth exploring the limits of software and technology. 
+            I have since then expanded and strengthened my skills within languages
+            such as: Python, JavaScript, HTML, and CSS, with knowledge in libraries 
+            and frameworks such as Djano, ReactJS, Pandas, OpenPyxl and more.
+        </p>
+        <p className='paragraph paragraph-2'>
+          I had begun my journey of software development by writing small 
+          scripts with Python, ranging from menial tasks to small games. From then on I had 
+          found my passion within the world of technology and engineering, ultimately influencing 
+          my decision to enter the field of engineering at UAlberta. What I aim to accomplish within 
+          my lifetime involves advancements and contributions within machine learning and artificial 
+          intelligence. What I ultimately believe, is that as a collective we must come to embrace and 
+          rely upon these technological advancements in order to truly progress and evolve as humans. 
+          The limitless boundary of the universe cannot possibly be contained, nor examined by us alone... 
+          but thats where the possibilities of AI come in.
+        </p>
+        <p className='paragraph paragraph-3'>
+          Aside from my professional endeavors, I have been greatly passionate about fitness and 
+          personal wellness. I have consistently pursued fitness since highschool, recently seeking 
+          to branch out into a mix between powerliting and bodybuilding. I have since built structured 
+          and thought out regiments for a number of my peers looking to enter the fitness world. 
+          A dream of mine would be to ideally combine my passions within software and tech development 
+          with health and wellness. A future project that I have begun recently includes an application 
+          incorporating software into fitness. The program seamlessly takes user input in regards to their health, 
+          fitness, and nutritional goals. Using this information, the software would strategically output 
+          scientifically accurate and backed regiments to help users attain their goals.
+        </p>
       </div>
     </section>
   )
