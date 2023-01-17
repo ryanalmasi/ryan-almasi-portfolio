5caf340c550d46ed2276a9d725d67debf62c786f
ryanalmasi <ryanalmasii@gmail.com>
2023-01-17 00:23:51 -0700

    Changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,12 +15,6 @@ const About = () => {
       </header>
       
       <div className='container'>
-        <div className='container about-cont'>
-          <div className='about_img'>
-            <img src={IMAGE} alt='My Image' />
-          </div>
-        </div>
-
         <div className='about-cont_content'>
           <div className='about-cards'>
             <article className='about-card about-card-experience'>
@@ -41,37 +35,46 @@ const About = () => {
               <small>4 months | Summer 2023</small>
             </article>
           </div>
+          
+          <div className='about-cont-footer'>
+            <div className='about-cont'>
+              <div className='about_img'>
+                <img src={IMAGE} alt='My Image' />
+              </div>
+            </div>
+
+            <div className='paragraph-cont'>
+              <p className='paragraph paragraph-1'>
+                  Developer since youth exploring the limits of software and technology. 
+                  I have since then expanded and strengthened my skills within languages
+                  such as: Python, JavaScript, HTML, and CSS, with knowledge in libraries 
+                  and frameworks such as Djano, ReactJS, Pandas, OpenPyxl and more.
+              </p>
+              <p className='paragraph paragraph-2'>
+                I had begun my journey of software development by writing small 
+                scripts with Python, ranging from menial tasks to small games. From then on I had 
+                found my passion within the world of technology and engineering, ultimately influencing 
+                my decision to enter the field of engineering at UAlberta. What I aim to accomplish within 
+                my lifetime involves advancements and contributions within machine learning and artificial 
+                intelligence. What I ultimately believe, is that as a collective we must come to embrace and 
+                rely upon these technological advancements in order to truly progress and evolve as humans. 
+                The limitless boundary of the universe cannot possibly be contained, nor examined by us alone... 
+                but thats where the possibilities of AI come in.
+              </p>
+              <p className='paragraph paragraph-3'>
+                Aside from my professional endeavors, I have been greatly passionate about fitness and 
+                personal wellness. I have consistently pursued fitness since highschool, recently seeking 
+                to branch out into a mix between powerliting and bodybuilding. I have since built structured 
+                and thought out regiments for a number of my peers looking to enter the fitness world. 
+                A dream of mine would be to ideally combine my passions within software and tech development 
+                with health and wellness. A future project that I have begun recently includes an application 
+                incorporating software into fitness. The program seamlessly takes user input in regards to their health, 
+                fitness, and nutritional goals. Using this information, the software would strategically output 
+                scientifically accurate and backed regiments to help users attain their goals.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='paragraph-cont'>
-        <p className='paragraph paragraph-1'>
-            Developer since youth exploring the limits of software and technology. 
-            I have since then expanded and strengthened my skills within languages
-            such as: Python, JavaScript, HTML, and CSS, with knowledge in libraries 
-            and frameworks such as Djano, ReactJS, Pandas, OpenPyxl and more.
-        </p>
-        <p className='paragraph paragraph-2'>
-          I had begun my journey of software development by writing small 
-          scripts with Python, ranging from menial tasks to small games. From then on I had 
-          found my passion within the world of technology and engineering, ultimately influencing 
-          my decision to enter the field of engineering at UAlberta. What I aim to accomplish within 
-          my lifetime involves advancements and contributions within machine learning and artificial 
-          intelligence. What I ultimately believe, is that as a collective we must come to embrace and 
-          rely upon these technological advancements in order to truly progress and evolve as humans. 
-          The limitless boundary of the universe cannot possibly be contained, nor examined by us alone... 
-          but thats where the possibilities of AI come in.
-        </p>
-        <p className='paragraph paragraph-3'>
-          Aside from my professional endeavors, I have been greatly passionate about fitness and 
-          personal wellness. I have consistently pursued fitness since highschool, recently seeking 
-          to branch out into a mix between powerliting and bodybuilding. I have since built structured 
-          and thought out regiments for a number of my peers looking to enter the fitness world. 
-          A dream of mine would be to ideally combine my passions within software and tech development 
-          with health and wellness. A future project that I have begun recently includes an application 
-          incorporating software into fitness. The program seamlessly takes user input in regards to their health, 
-          fitness, and nutritional goals. Using this information, the software would strategically output 
-          scientifically accurate and backed regiments to help users attain their goals.
-        </p>
       </div>
     </section>
   )
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,71 @@
 import React from "react";
+import './Projects.scss'
 
 
 const Projects = () => {
     return (
-        <div>HELLO!</div>
+        <section id="projects">
+            <div className="portfolio-container">
+                <div className="projects-header">
+                    <h1>PROJECTS</h1>
+                    <h2>&lt; /&gt;</h2>
+                </div>
+
+                <div className="items">
+                    <div className="items-wrapper">
+                        <article className="portfolio-item">
+                            <div className="portfolio-img">
+                                <img alt='' />
+                            </div>
+                            <h3>CompanyStocker</h3>
+                            <div className="portfolio-item-cta">
+                                <a href='https://github.com/ryanalmasi/CompanyStocker' className="btn">TAKE A LOOK</a>
+                            </div>
+                        </article>
+
+                        <article className="portfolio-item">
+                            <div className="portfolio-img">
+                                <img alt='' />
+                            </div>
+                            <h3>Timesheet Automator</h3>
+                            <div className="portfolio-item-cta">
+                                <a href='https://github.com/ryanalmasi/Basic-Automated-Time-Sheet' className="btn">TAKE A LOOK</a>
+                            </div>
+                        </article>
+
+                        <article className="portfolio-item">
+                            <div className="portfolio-img">
+                                <img alt='' />
+                            </div>
+                            <h3>Currency Conversion App</h3>
+                            <div className="portfolio-item-cta">
+                                <a href='https://github.com/ryanalmasi/Currency-Converter-App' className="btn">TAKE A LOOK</a>
+                            </div>
+                        </article>
+
+                        <article className="portfolio-item">
+                            <div className="portfolio-img">
+                                <img alt='' />
+                            </div>
+                            <h3>Receipt Scanner with OCR API</h3>
+                            <div className="portfolio-item-cta">
+                                <a href='https://github.com/ryanalmasi/ryan-almasi-portfolio' className="btn">TAKE A LOOK</a>
+                            </div>
+                        </article>
+
+                        <article className="portfolio-item">
+                            <div className="portfolio-img">
+                                <img alt='' />
+                            </div>
+                            <h3>This Website</h3>
+                            <div className="portfolio-item-cta">
+                                <a href='https://github.com/ryanalmasi/ReceiptScannerProject' className="btn">TAKE A LOOK</a>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
