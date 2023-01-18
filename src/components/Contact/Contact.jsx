9d362ac0a5cc14b1713056ceae20f1854087d5ca
ryanalmasi <108089503+ryanalmasi@users.noreply.github.com>
2023-01-17 17:58:36 -0700

    Changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './Contact.scss'
+import { AiOutlineMail, AiOutlineLinkedin } from 'react-icons/ai'
+import { FiGithub } from 'react-icons/fi'
 
 
 const Contact = () => {
@@ -12,8 +14,26 @@ const Contact = () => {
                 </div>
 
                 <div className="contact-content">
-                    <h3 className="contact-h3">THANK YOU FOR SCROLLING</h3>
-                    <h4 className="contact-h4">PLEASE FEEL FREE TO CONTACT ME THROUGH ANY OF THE PLATFORMS LISTED ABOVE OR THROUGH MY EMAIL</h4>
+                    <article className="contact-cards">
+                        <AiOutlineMail className="contact-icon" />
+                        <h3>EMAIL</h3>
+                        <h4>ryan.almasi@gmail.com</h4>
+                        <a href="mailto: ryan.almasi@gmail.com" target='blank' className="btn contact-btn">EMAIL ME</a>
+                    </article>
+
+                    <article className="contact-cards">
+                        <AiOutlineLinkedin className="contact-icon" />
+                        <h3>LINKEDIN</h3>
+                        <h4>Ryan Almasi</h4>
+                        <a href="https://www.linkedin.com/in/ryan-almasi-493447251/" target='_blank' className="btn contact-btn">CONNECT WITH ME</a>
+                    </article>
+
+                    <article className="contact-cards">
+                        <FiGithub className="contact-icon" />
+                        <h3>GITHUB</h3>
+                        <h4>ryanalmasi</h4>
+                        <a href="mailto: ryan.almasi@gmail.com" target='blank' className="btn contact-btn">EMAIL ME</a>
+                    </article>
                 </div>
             </div>
         </section>
